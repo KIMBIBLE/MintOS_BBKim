5724b16f0a057c77ec36b6d29bbc91cd01ff4e69
jotun9935 <jotun9935@gmail.com>
2018-01-16 19:08:08 +0900

    edit : virtual OS image for test

--- 01.Kernel32/VirtualOS.asm
+++ 01.Kernel32/VirtualOS.asm
@@ -5,20 +5,36 @@ SECTION .text
 
 jmp 0x1000:START
 
-SECTORCOUNT:	dw 0x0000
+SECTORCOUNT:		dw	0x0000
+TOTALSECTORCOUNT	equ	1024
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Code Section
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 START:
 	mov ax, cs
 	mov ds, ax
 	mov ax, 0xB800
 	mov es, ax
 
-	mov ax, 2
-	mul word [SECTORCOUNT]
-	mov si, ax
-	mov byte [es:si + (160 * 2)], '0'
-	add word [SECTORCOUNT], 1
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	; Data Section
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	%assign i 0
+	%rep TOTALSECTORCOUNT
+		%assign i i+1
 
-	jmp $
+		mov ax, 2
+		mul word [SECTORCOUNT]
+		mov si, ax
+		mov byte [es:si + (160 * 2)], '0' + (i % 10)
+		add word [SECTORCOUNT], 1
 
-	times 512 -($ - $$) db 0x00
+		%if i == TOTALSECTORCOUNT
+			jmp $
+		%else
+			jmp (0x1000 + i * 0x20): 0x0000
+		%endif
+
+		times (512 -($ - $$) % 512) db 0x00
+%endrep
